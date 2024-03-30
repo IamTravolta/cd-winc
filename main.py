e32826b469e07b535f38c2238517152c5af76c97
IamTravolta <104280452+IamTravolta@users.noreply.github.com>
2024-03-30 04:00:56 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,25 +1,17 @@
 # Import what we need from flask
 # from flask import Flask, render_template
-# from lib import power
-
-# Create a Flask app inside `app`
-# app = Flask(__name__)
-
-# Assign a function to be called when the path `/` is requested
-# @app.route('/')
-# def index():
-#     return 'MOoooOo again!'
-
-
 from flask import Flask
 from lib import power
 
+# Create a Flask app inside `app`
 app = Flask(__name__)
 
+# Assign a function to be called when the path `/` is requested
 @app.route('/')
 def index():
     result = power(2, 2)
     return f'The result is {result}'
+
 
 if __name__ == '__main__':
     app.run(debug=True)
