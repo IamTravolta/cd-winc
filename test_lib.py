ed3bc98c351f7f0b2052b7b944a994232c03d5ed
IamTravolta <104280452+IamTravolta@users.noreply.github.com>
2024-03-30 03:46:56 +0100

    Update test_lib.py

--- test_lib.py
+++ test_lib.py
@@ -1,5 +1,16 @@
 from flask import lib
 
 def test_power():
-    assert lib.power(2,2) == 4
-    assert lib.power(2,3) == 8
+    assert lib.power(2, 2) == 4
+    assert lib.power(2, 3) == 8
+
+
+
+
+# test_app.py
+
+from lib import power
+
+def test_power():
+    assert power(2, 2) == 4
+    assert power(2, 3) == 8
