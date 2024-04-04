a9f9ad9b6d8bd245b5b6360f0fbe8439d1cfc0fd
IamTravolta <104280452+IamTravolta@users.noreply.github.com>
2024-04-04 12:55:03 +0200

    Update commands.sh

--- commands.sh
+++ commands.sh
@@ -1,8 +1,7 @@
 cd /home/farm
 
 git add .
-git clone https://github.com/IamTravolta/cd-winc
-git branch --set-upstream-to=origin/main main
+git clone git@github.com:M https://github.com/IamTravolta/cd-winc
 git pull
 systemctl restart farm
 exit
