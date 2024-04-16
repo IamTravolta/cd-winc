2a02feaa33e76f4ab37060a1edb44c9cf8244f94
IamTravolta <104280452+IamTravolta@users.noreply.github.com>
2024-04-16 05:12:39 +0200

    Update commands.sh

--- commands.sh
+++ commands.sh
@@ -10,7 +10,7 @@ cd /home/farm
 # Configure Git to know which remote branch to pull from
 git remote add origin https://github.com/IamTravolta/cd-winc.git
 git fetch --all
-git branch --set-upstream-to=origin/main 
+git branch --set-upstream-to=origin/main farm
 
 # Pull the latest changes
 git pull origin main
